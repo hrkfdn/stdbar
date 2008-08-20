bc4d60fb6e45bcc4201a9b42d4ab19b9a3c39e4c
Henrik Friedrichsen <hrkfrd@gmail.com>
2008-08-21 00:52:31 +0200

    example eventhandler

--- events.c
+++ events.c
@@ -1,7 +1,27 @@
+#include <stdbool.h>
 #include <X11/Xlib.h>
+
+extern Display* dpy;
+
+extern bool running;
+
+void
+ev_expose()
+{
+
+}
 
 void
 eventloop()
 {
-
+	XEvent ev;
+	while(running && !XNextEvent(dpy, &ev)) {
+		switch(ev.type) {
+			case Expose:
+				ev_expose();
+				break;
+			default:
+				break;
+		}
+	}
 }
