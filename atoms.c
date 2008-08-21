211a5175f9acd6d9cc306143bfe4fa476c9ea7ee
Henrik Friedrichsen <hrkfrd@gmail.com>
2008-08-22 01:13:48 +0200

    setup _NET_WM_NAME atom

--- atoms.c
+++ atoms.c
@@ -3,13 +3,17 @@
 #define CREATEATOM(ident, varname) atom_t varname = { #ident, 0 }
 #define INITATOM(varname) varname.atom = XInternAtom(dpy, varname.name, False)
 
+char wmname[] = "stdbar "VERSION;
+
 typedef struct
 {
 	char* name;
 	Atom atom;
 } atom_t;
 
-CREATEATOM(_NET_SUPPORTED, net_supported);
+CREATEATOM(UTF8_STRING, utf8_string);
+
+CREATEATOM(_NET_WM_NAME, net_wm_name);
 CREATEATOM(_NET_WM_WINDOW_TYPE, net_wm_window_type);
 CREATEATOM(_NET_WM_WINDOW_TYPE_DOCK, net_wm_window_type_dock);
 CREATEATOM(_NET_WM_STRUT, net_wm_strut);
@@ -18,7 +22,9 @@ CREATEATOM(_NET_WM_STRUT_PARTIAL, net_wm_strut_partial);
 void
 initatoms()
 {
-	INITATOM(net_supported);
+	INITATOM(utf8_string);
+
+	INITATOM(net_wm_name);
 	INITATOM(net_wm_window_type);
 	INITATOM(net_wm_window_type_dock);
 	INITATOM(net_wm_strut);
@@ -39,4 +45,6 @@ setatoms(int barh)
 	XChangeProperty(dpy, win, net_wm_strut.atom, XA_CARDINAL, 32, PropModeReplace, (unsigned char*)struts, 4); // for compatibility reasons
 	XChangeProperty(dpy, win, net_wm_strut_partial.atom, XA_CARDINAL, 32, PropModeReplace, (unsigned char*)struts, 12);
 	XChangeProperty(dpy, win, net_wm_window_type.atom, XA_ATOM, 32, PropModeReplace, (unsigned char*)&net_wm_window_type_dock.atom, 1);
+
+	XChangeProperty(dpy, win, net_wm_name.atom, utf8_string.atom, 8, PropModeReplace, (unsigned char*)wmname, strlen(wmname));
 }
